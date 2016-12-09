48fa02730f64292c000884e248b8a48a8c3b3e41
bobbbbby <17slutz@greenwichcsd.org>
2016-12-08 20:07:49 -0500

    just #

---
+++ src/FunNumbers.java
@@ -0,0 +1,42 @@
+
+public class FunNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int iVal = 100;
+//		int iVal2 = iVal;
+//		System.out.println(iVal);
+//		System.out.println(iVal2);
+//		iVal = 300;
+//		System.out.println(iVal);
+//		System.out.println(iVal2);
+		
+//		byte bVal3 = 100;
+//		long lVal = 123456789;
+//				
+//		lVal = bVal3;
+//		bVal3 = (byte)lVal;
+//				
+//		float fVal1 = 13;
+//		float fVal2 = 5;
+//		
+//		System.out.println(fVal1/fVal2);
+//		
+//		float sVal1 = 13;
+//		float sVal2 = 5;
+//		
+//		System.out.println(sVal1/sVal2);
+//		
+//		System.out.println(fVal1/sVal2);
+		
+		int newVal = 5;
+		System.out.println(newVal++);
+		System.out.println(newVal);
+		System.out.println(++newVal);
+		System.out.println(newVal+38);		
+		
+		System.out.println(newVal+= 5);
+		System.out.println(newVal);
+	}
+
+}
